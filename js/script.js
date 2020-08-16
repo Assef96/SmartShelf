c52c3d75bce31c1b398e8f85ab28be325aeb5b6c
assef96 <aghamisi@gmail.com>
2020-08-16 14:10:07 +0430

    Frontend Improved

--- js/script.js
+++ js/script.js
@@ -1,15 +1,9 @@
-var units_product_id = [0, 0, 0, 0, 0];
-
-
 window.onload = function() {
-    load_shelf_status();
     load_shelf();
+    load_shelf_status();
     load_commands();
-    // load_products_list();
-    // load_product();
 };
 window.setInterval(function() {
-    // load_ambient();
     // load_shelf_status();
 }, 200);
 
@@ -17,7 +11,6 @@ window.setInterval(function() {
 function load_shelf_status() {
     var url = "api/read_last_ambient.php"
     $.getJSON(url, function(response) {
-        // var val = response;
         var humidity = (response['ambient']['humidity']);
         var temperature = (response['ambient']['temperature']);
         var light = (response['ambient']['light']);
@@ -78,6 +71,19 @@ function load_shelf_status() {
     });
 }
 
+function load_shelf() {
+    var url = "api/read_units_products.php"
+    $.getJSON(url, function(response) {
+        for (let index = 0; index < response["units"].length; index++) {
+            const unit = response["units"][index];
+            // $(".pic.unit".concat(unit["id"])).attr("src", "img/placeholder.png");
+            $(".pic.unit".concat(unit["id"])).attr("src", "img/".concat(unit["image"]));
+            $(".pic.unit".concat(unit["id"])).attr("alt", unit["name"]);
+            $("#unit-info".concat(unit["id"])).html(unit["name"]);
+        }
+    });
+}
+
 function load_commands() {
     var url = "api/read_commands.php"
     $.getJSON(url, function(response) {
@@ -96,121 +102,16 @@ function load_commands() {
     });
 }
 
-function load_shelf() {
-    var url = "api/read_units_products.php"
-    $.getJSON(url, function(response) {
-        for (let index = 0; index < response["units"].length; index++) {
-            const unit = response["units"][index];
-            // $(".pic.unit".concat(unit["id"])).attr("src", "img/placeholder.png");
-            $(".pic.unit".concat(unit["id"])).attr("src", "img/".concat(unit["image"]));
-            $(".pic.unit".concat(unit["id"])).attr("alt", unit["name"]);
-            $("#unit-info".concat(unit["id"])).html(unit["name"]);
-            units_product_id[index] = unit["product_id"];
-        }
+function update_commands(component_id) {
+    var url = "api/update_commands.php";
+    var checkBox = document.getElementById(component_id);
+    var status = 0;
+    if (checkBox.checked == true)
+    status = 1;
+    $.post(url, { [component_id]: status }, function(result) {
+        console.log(result);
     });
 }
-
-function load_products_list() {
-    var url = "api/read_products.php"
-    $.getJSON(url, function(response) {
-        var str = '';
-        for (let index = 0; index < response["products"].length; index++) {
-            const product = response["products"][index];
-            const id = product["id"];
-            const name = product["name"];
-            const price = product["price"];
-            const image = product["image"];
-
-            // $(".pic.unit".concat(unit["id"])).attr("src", "img/placeholder.png");
-            // $(".pic.unit".concat(unit["id"])).attr("src", "img/".concat(unit["image"]));
-            // $(".pic.unit".concat(unit["id"])).attr("alt", unit["name"]);
-            // $("#unit-info".concat(unit["id"])).html(unit["name"]);
-            str += `<div class="card" style="width: 15rem;">
-            <img src="img/${image}" class="card-img-top" alt="${image}">
-            <div class="card-body">
-            <h5 class="card-title">${name}</h5>
-            <p class="card-text">id: ${id}</p>
-            <p class="card-text">Price: ${price}</p>
-            <a href="#" class="btn btn-primary">Go somewhere</a>
-            </div>
-            </div>`;
-            $("#products-list").html(str);
-        }
-    });
-}
-
-
-
-
-$('#lamp1').on('click', function() {
-    var url = "api/update_commands.php";
-    var checkBox = document.getElementById("lamp1");
-    var status = 0;
-    if (checkBox.checked == true)
-        status = 1;
-    $.post(url, { "lamp1": status }, function(result) {
-        console.log(result);
-    });
-});
-
-$('#lamp2').on('click', function() {
-    var url = "api/update_commands.php";
-    var checkBox = document.getElementById("lamp2");
-    var status = 0;
-    if (checkBox.checked == true)
-        status = 1;
-    $.post(url, { "lamp2": status }, function(result) {
-        console.log(result);
-    });
-});
-
-$('#buzzer').on('click', function() {
-    var url = "api/update_commands.php";
-    var checkBox = document.getElementById("buzzer");
-    var status = 0;
-    if (checkBox.checked == true)
-        status = 1;
-    $.post(url, { "buzzer": status }, function(result) {
-        console.log(result);
-    });
-});
-
-
-$('#led-on').on('click', function() {
-    var url = "api/update_actuators.php?status=on";
-    $.getJSON(url, function(response) {
-        console.log(response);
-    });
-});
-
-$('#led-off').on('click', function() {
-    var url = "api/update_actuators.php?status=off";
-    $.getJSON(url, function(response) {
-        console.log(response);
-    });
-});
-
-$('#alarm-on').on('click', function() {
-    var url = "api/update_actuators.php?alarm=on";
-    $.getJSON(url, function(response) {
-        console.log(response);
-    });
-});
-
-$('#alarm-off').on('click', function() {
-    var url = "api/update_actuators.php?alarm=off";
-    $.getJSON(url, function(response) {
-        console.log(response);
-    });
-});
-
-$('#alarm-off').on('click', function() {
-    var url = "api/update_actuators.php?alarm=off";
-    $.getJSON(url, function(response) {
-        console.log(response);
-    });
-});
-
 
 function show_products(unitId) {
     var url = `products.html?u=${unitId}`;
